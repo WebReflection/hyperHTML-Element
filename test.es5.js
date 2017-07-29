1c01bc39142f1f485e8eb00bf153a22ba968c2b7
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-07-29 09:36:29 +0100

    tarnspiling only natives

--- test.es5.js
+++ test.es5.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _templateObject = _taggedTemplateLiteral(['\n    Hi, my name is ', ' and I am ', ''], ['\n    Hi, my name is ', ' and I am ', '']),
@@ -15,32 +13,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _fixBabelExtend = function (O) {
-  var gOPD = O.getOwnPropertyDescriptor,
-      gPO = O.getPrototypeOf || function (o) {
-    return o.__proto__;
-  },
-      sPO = O.setPrototypeOf || function (o, p) {
-    o.__proto__ = p;
-    return o;
-  },
-      construct = (typeof Reflect === 'undefined' ? 'undefined' : _typeof(Reflect)) === 'object' ? Reflect.construct : function (Parent, args, Class) {
-    var Constructor,
-        a = [null];
-    a.push.apply(a, args);
-    Constructor = Parent.bind.apply(Parent, a);
-    return sPO(new Constructor(), Class.prototype);
-  };
-
-  return function fixBabelExtend(Class) {
-    var Parent = gPO(Class);
-    return sPO(Class, sPO(function Super() {
-      return construct(Parent, arguments, gPO(this).constructor);
-    }, Parent));
-  };
-}(Object);
-
-var MySelf = _fixBabelExtend(function (_HyperHTMLElement) {
+var MySelf = function (_HyperHTMLElement) {
   _inherits(MySelf, _HyperHTMLElement);
 
   function MySelf() {
@@ -72,11 +45,11 @@ var MySelf = _fixBabelExtend(function (_HyperHTMLElement) {
   }]);
 
   return MySelf;
-}(HyperHTMLElement));
+}(HyperHTMLElement);
 
 MySelf.define('my-self');
 
-var MyInput = _fixBabelExtend(function (_HyperHTMLElement2) {
+var MyInput = function (_HyperHTMLElement2) {
   _inherits(MyInput, _HyperHTMLElement2);
 
   function MyInput() {
@@ -109,7 +82,7 @@ var MyInput = _fixBabelExtend(function (_HyperHTMLElement2) {
   }]);
 
   return MyInput;
-}(HyperHTMLElement));
+}(HyperHTMLElement);
 
 MyInput.define('my-input');
 
