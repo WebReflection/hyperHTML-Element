2285cc7bec0efe2ed36be779980a08fdf1bd9b99
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-05-24 12:54:09 +0200

    Updated to latest hyperHTML

--- es5.js
+++ es5.js
@@ -946,15 +946,14 @@ var HyperHTMLElement = (function (exports) {
   /*! (c) Andrea Giammarchi */
   function disconnected(poly) {
 
-    var CONNECTED = 'connected';
-    var DISCONNECTED = 'dis' + CONNECTED;
     var Event = poly.Event;
     var WeakSet = poly.WeakSet;
     var notObserving = true;
-    var observer = new WeakSet();
+    var observer = null;
     return function observe(node) {
       if (notObserving) {
         notObserving = !notObserving;
+        observer = new WeakSet();
         startObserving(node.ownerDocument);
       }
 
@@ -963,7 +962,8 @@ var HyperHTMLElement = (function (exports) {
     };
 
     function startObserving(document) {
-      var dispatched = null;
+      var connected = new WeakSet();
+      var disconnected = new WeakSet();
 
       try {
         new MutationObserver(changes).observe(document, {
@@ -997,26 +997,22 @@ var HyperHTMLElement = (function (exports) {
       }
 
       function changes(records) {
-        dispatched = new Tracker();
-
         for (var record, length = records.length, i = 0; i < length; i++) {
           record = records[i];
-          dispatchAll(record.removedNodes, DISCONNECTED, CONNECTED);
-          dispatchAll(record.addedNodes, CONNECTED, DISCONNECTED);
-        }
-
-        dispatched = null;
-      }
-
-      function dispatchAll(nodes, type, counter) {
-        for (var node, event = new Event(type), length = nodes.length, i = 0; i < length; (node = nodes[i++]).nodeType === 1 && dispatchTarget(node, event, type, counter)) {
+          dispatchAll(record.removedNodes, 'disconnected', disconnected, connected);
+          dispatchAll(record.addedNodes, 'connected', connected, disconnected);
         }
       }
 
-      function dispatchTarget(node, event, type, counter) {
-        if (observer.has(node) && !dispatched[type].has(node)) {
-          dispatched[counter]["delete"](node);
-          dispatched[type].add(node);
+      function dispatchAll(nodes, type, wsin, wsout) {
+        for (var node, event = new Event(type), length = nodes.length, i = 0; i < length; (node = nodes[i++]).nodeType === 1 && dispatchTarget(node, event, type, wsin, wsout)) {
+        }
+      }
+
+      function dispatchTarget(node, event, type, wsin, wsout) {
+        if (observer.has(node) && !wsin.has(node)) {
+          wsout["delete"](node);
+          wsin.add(node);
           node.dispatchEvent(event);
           /*
           // The event is not bubbling (perf reason: should it?),
@@ -1035,13 +1031,8 @@ var HyperHTMLElement = (function (exports) {
 
         for (var // apparently is node.children || IE11 ... ^_^;;
         // https://github.com/WebReflection/disconnected/issues/1
-        children = node.children || [], length = children.length, i = 0; i < length; dispatchTarget(children[i++], event, type, counter)) {
+        children = node.children || [], length = children.length, i = 0; i < length; dispatchTarget(children[i++], event, type, wsin, wsout)) {
         }
-      }
-
-      function Tracker() {
-        this[CONNECTED] = new WeakSet();
-        this[DISCONNECTED] = new WeakSet();
       }
     }
   }
--- index.js
+++ index.js
@@ -962,22 +962,22 @@ var HyperHTMLElement = (function (exports) {
   }(document));
 
   /*! (c) Andrea Giammarchi */
-  function disconnected(poly) {  var CONNECTED = 'connected';
-    var DISCONNECTED = 'dis' + CONNECTED;
-    var Event = poly.Event;
+  function disconnected(poly) {  var Event = poly.Event;
     var WeakSet = poly.WeakSet;
     var notObserving = true;
-    var observer = new WeakSet;
+    var observer = null;
     return function observe(node) {
       if (notObserving) {
         notObserving = !notObserving;
+        observer = new WeakSet;
         startObserving(node.ownerDocument);
       }
       observer.add(node);
       return node;
     };
     function startObserving(document) {
-      var dispatched = null;
+      var connected = new WeakSet;
+      var disconnected = new WeakSet;
       try {
         (new MutationObserver(changes)).observe(
           document,
@@ -1013,32 +1013,30 @@ var HyperHTMLElement = (function (exports) {
         );
       }
       function changes(records) {
-        dispatched = new Tracker;
         for (var
           record,
           length = records.length,
           i = 0; i < length; i++
         ) {
           record = records[i];
-          dispatchAll(record.removedNodes, DISCONNECTED, CONNECTED);
-          dispatchAll(record.addedNodes, CONNECTED, DISCONNECTED);
+          dispatchAll(record.removedNodes, 'disconnected', disconnected, connected);
+          dispatchAll(record.addedNodes, 'connected', connected, disconnected);
         }
-        dispatched = null;
       }
-      function dispatchAll(nodes, type, counter) {
+      function dispatchAll(nodes, type, wsin, wsout) {
         for (var
           node,
           event = new Event(type),
           length = nodes.length,
           i = 0; i < length;
           (node = nodes[i++]).nodeType === 1 &&
-          dispatchTarget(node, event, type, counter)
+          dispatchTarget(node, event, type, wsin, wsout)
         );
       }
-      function dispatchTarget(node, event, type, counter) {
-        if (observer.has(node) && !dispatched[type].has(node)) {
-          dispatched[counter].delete(node);
-          dispatched[type].add(node);
+      function dispatchTarget(node, event, type, wsin, wsout) {
+        if (observer.has(node) && !wsin.has(node)) {
+          wsout.delete(node);
+          wsin.add(node);
           node.dispatchEvent(event);
           /*
           // The event is not bubbling (perf reason: should it?),
@@ -1060,12 +1058,8 @@ var HyperHTMLElement = (function (exports) {
           children = node.children || [],
           length = children.length,
           i = 0; i < length;
-          dispatchTarget(children[i++], event, type, counter)
+          dispatchTarget(children[i++], event, type, wsin, wsout)
         );
-      }
-      function Tracker() {
-        this[CONNECTED] = new WeakSet;
-        this[DISCONNECTED] = new WeakSet;
       }
     }
   }
